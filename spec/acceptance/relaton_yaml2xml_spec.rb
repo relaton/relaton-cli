d7d985663322de2e5a68a8dc7a15ead5d4cc55c9
Abu Nashir <abunashir@gmail.com>
2018-11-08 15:13:19 +0100

    Cleanup the `relaton yaml2xml` interface
    The `yaml2xml` is dealing with quite some repetitive stuff, some
    other interface is also dependent on this one, so ideally it make
    sense to extract it out to it's own class and also add proper test
    to ensure it functioning as expected.
    
    For now, this commit extract it out to a simple class that expose
    a public interface, and under the hood it takes care of all of the
    necessary steps. Since, it's in it's own so it also makes it much
    more easier to test.
    
    Another noticeable thing, whenever we are running our test suite
    then it also writes a lot of file to the file system, which also
    creates some occasional problem, so this commit also changes it
    so we don't write the content directly to a file for test but
    capture it in a string buffer and then run test on those.

---
+++ spec/acceptance/relaton_yaml2xml_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+RSpec.describe "Relaton yaml2xml" do
+  describe "relaton yaml2xml" do
+    context "yaml file without any option" do
+      it "sends convertion message to the convertaor" do
+        allow(Relaton::Cli::YAMLConvertor).to receive(:to_xml)
+
+        command = %w(yaml2xml spec/fixtures/sample.yaml)
+        Relaton::Cli.start(command)
+
+        expect(Relaton::Cli::YAMLConvertor).to have_received(:to_xml).
+          with("spec/fixtures/sample.yaml", {})
+      end
+    end
+
+    context "yaml file with options" do
+      it "sends convertion message with file and options" do
+        allow(Relaton::Cli::YAMLConvertor).to receive(:to_xml)
+
+        command = %w(yaml2xml spec/fixtures/sample.yaml -x rxml -p RCL)
+        Relaton::Cli.start(command)
+
+        expect(Relaton::Cli::YAMLConvertor).to have_received(:to_xml).
+          with("spec/fixtures/sample.yaml", extension: "rxml", prefix: "RCL")
+      end
+    end
+  end
+end
