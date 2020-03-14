2bdec673ff5f4568bb7dc38ff7b17af522253005
Andrei Kislichenko <android.2net@gmail.com>
2020-03-14 11:26:20 +0100

    add relaton-un

--- spec/acceptance/relaton_fetch_spec.rb
+++ spec/acceptance/relaton_fetch_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe "Relaton Fetch" do
     context "fetch code with invalid/missing type" do
       it "calls supported_type_message method" do
         io = double "IO"
-        expect(io).to receive(:puts).with "Recognised types: CC, CN, IEC, IETF, ISO, ITU, NIST, OGC, OMG"
+        expect(io).to receive(:puts).with "Recognised types: CC, CN, IEC, IETF, ISO, ITU, NIST, OGC, OMG, UN"
         expect(IO).to receive(:new).with(kind_of(Integer), mode: 'w:UTF-8').and_return io
         command = Relaton::Cli::Command.new
         command.fetch ["ISO 2146", "--type", "invalid"]
