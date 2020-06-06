1f4985ac37075b14b3edb1e801a411db4272a0a6
IEUser <abobrikovich@gmail.com>
2020-06-06 08:49:27 -0700

    #79 Move to %x and Open3.capture3 from rspec's command

--- spec/acceptance/relaton_fetch_spec.rb
+++ spec/acceptance/relaton_fetch_spec.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 RSpec.describe "Relaton Fetch" do
   describe "relaton fetch" do
     it "calls fetch" do
@@ -11,29 +13,29 @@ RSpec.describe "Relaton Fetch" do
 
     context "fetch code with a type" do
       it "prints out the document for valid code and type" do
-        output = command("relaton fetch --type ISO 'ISO 2146'")
+        output = %x[relaton fetch --type ISO 'ISO 2146']
 
-        expect(output.stdout).to include('<relation type="obsoletes">')
-        expect(output.stdout).to include('<docidentifier type="ISO">ISO 2146')
+        expect(output).to include('<relation type="obsoletes">')
+        expect(output).to include('<docidentifier type="ISO">ISO 2146')
       end
 
       it "prints out the document in BibTeX format" do
-        output = command("relaton fetch --format bibtex --type ISO 'ISO 2146'")
-        expect(output.stdout).to include "@misc{ISO2146(allparts),"
+        output = %x[relaton fetch --format bibtex --type ISO 'ISO 2146']
+        expect(output).to include "@misc{ISO2146(allparts),"
       end
     end
 
     context "fetch code with date specified" do
       it "prints out the correct document for valid date" do
-        output = command("relaton fetch -t ISO -y 2010 'ISO 2146'")
+        output = %x[relaton fetch -t ISO -y 2010 'ISO 2146']
 
-        expect(output.stdout).to include('<relation type="obsoletes">')
-        expect(output.stdout).to include('<docidentifier type="ISO">ISO 2146')
+        expect(output).to include('<relation type="obsoletes">')
+        expect(output).to include('<docidentifier type="ISO">ISO 2146')
       end
 
       it "prints out a warning messages for wrong date" do
-        output = command("relaton fetch -t ISO -y 2009 'ISO 2146'")
-        expect(output.stdout).to include("No matching bibliographic entry")
+        output = %x[relaton fetch -t ISO -y 2009 'ISO 2146']
+        expect(output).to include("No matching bibliographic entry")
       end
     end
 
@@ -49,13 +51,13 @@ RSpec.describe "Relaton Fetch" do
       end
 
       it "prints a warning message for missing --type option" do
-        output = command("relaton fetch 'ISO 2146'")
-        expect(output.stderr).to include("required options '--type'")
+        _, stderr, _ = Open3.capture3("relaton fetch 'ISO 2146'")
+        expect(stderr).to include("required options '--type'")
       end
 
       it "prints a warning message with suggestions for invalid type" do
-        output = command("relaton fetch 'ISO 2146' --type invalid")
-        expect(output.stdout).to include(
+        output = %x[relaton fetch 'ISO 2146' --type invalid]
+        expect(output).to include(
           "Recognised types: CC, CN, IEC, IEEE, IETF, ISO, ITU, NIST, OGC, OMG, UN, W3C",
         )
       end
@@ -63,8 +65,8 @@ RSpec.describe "Relaton Fetch" do
 
     context "fetch code with undefined standard" do
       it "prints out a warning message for undefined standard" do
-        output = command("relaton fetch -t ISO 'ISO ABCDEFGH'")
-        expect(output.stdout).to include("No matching bibliographic entry foun")
+        output = %x[relaton fetch -t ISO 'ISO ABCDEFGH']
+        expect(output).to include("No matching bibliographic entry found")
       end
     end
 
