f5b3fc41202cffc4f7160bdb9b998a15f08e614b
Nick Nicholas <opoudjis@gmail.com>
2020-06-09 21:45:08 +1000

    make gem callable from other gems

--- lib/relaton-cli.rb
+++ lib/relaton-cli.rb
@@ -1,4 +1,10 @@
 require_relative "relaton/bibcollection"
 require_relative "relaton/bibdata"
 require_relative "relaton/element_finder"
-require_relative "relaton/xml_document"
+#require_relative "relaton/xml_document"
+require_relative "relaton/cli/yaml_convertor"
+
+module Relaton
+  module Cli
+  end
+end
