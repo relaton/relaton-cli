7e230b440aca0de71ef7e62b584ba69b79aa6fc6
Andrei Kislichenko <android.2net@gmail.com>
2021-09-08 17:24:29 +0200

    add cie-techstreet dataset

--- spec/acceptance/relaton_fetch_data_spec.rb
+++ spec/acceptance/relaton_fetch_data_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe "Relaton fetch-data" do
     command = %w[fetch-data nist-tech-pubs -o dir -f xml]
     Relaton::Cli.start command
   end
+
+  it "send techstreet message to DataFetcher" do
+    Relaton::Registry.instance
+    expect(RelatonCie::DataFetcher).to receive(:fetch)
+      .with output: "dir", format: "xml"
+    command = %w[fetch-data cie-techstreet -o dir -f xml]
+    Relaton::Cli.start command
+  end
 end
