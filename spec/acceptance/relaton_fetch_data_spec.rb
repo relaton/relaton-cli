15daf14b6e61c01c7b7f1d691111661d35dc1c42
Andrei Kislichenko <android.2net@gmail.com>
2021-09-09 16:35:14 +0200

    add test for calconnect data-fetch & upadte README

--- spec/acceptance/relaton_fetch_data_spec.rb
+++ spec/acceptance/relaton_fetch_data_spec.rb
@@ -7,11 +7,19 @@ RSpec.describe "Relaton fetch-data" do
     Relaton::Cli.start command
   end
 
-  it "send techstreet message to DataFetcher" do
+  it "send cie-techstreet message to DataFetcher" do
     Relaton::Registry.instance
     expect(RelatonCie::DataFetcher).to receive(:fetch)
       .with output: "dir", format: "xml"
     command = %w[fetch-data cie-techstreet -o dir -f xml]
+    Relaton::Cli.start command
+  end
+
+  it "send calconnect-org message to DataFetcher" do
+    Relaton::Registry.instance
+    expect(RelatonCalconnect::DataFetcher).to receive(:fetch)
+      .with output: "dir", format: "xml"
+    command = %w[fetch-data calconnect-org -o dir -f xml]
     Relaton::Cli.start command
   end
 end
