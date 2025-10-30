343b0ace138c532c3ced16f028fe898aafbbf4d2
KW Kwan <kw.kwan@ribose.com>
2025-10-30 13:38:37 +0800

    Adjust require order for proper dependency loading

--- lib/relaton-cli.rb
+++ lib/relaton-cli.rb
@@ -1,10 +1,12 @@
-require_relative "relaton/bibcollection"
-require_relative "relaton/bibdata"
-require_relative "relaton/element_finder"
-# require_relative "relaton/xml_document"
-require_relative "relaton/cli/yaml_convertor"
+require "relaton"
 
 module Relaton
   module Cli
   end
 end
+
+require_relative "relaton/bibcollection"
+require_relative "relaton/bibdata"
+require_relative "relaton/element_finder"
+require_relative "relaton/cli/yaml_convertor"
+require_relative "relaton/cli"
